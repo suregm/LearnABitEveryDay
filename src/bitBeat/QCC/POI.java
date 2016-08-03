a9810450ff9dc4b899e073dff2e4bf1dbc29ef6f
suregm <sure0kuaile@qq.com>
2016-08-03 23:49:44 +0800

    QCC knowledges.

---
+++ src/bitBeat/QCC/POI.java
@@ -0,0 +1,18 @@
+package bitBeat.QCC;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+
+/**
+ * Created by sure GM on 2016/8/3 23:43.
+ */
+public class POI {
+
+	File file = new File("D://yyzx.xlsx");
+
+	XSSFWorkbook wb = new XSSFWorkbook(file);
+
+	public void deleteStrikeRow()
+
+}
