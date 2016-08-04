260bf169621826bf9260df26c5b7f66b023e745a
suregm <sure0kuaile@qq.com>
2016-08-04 23:12:09 +0800

    QCC knowledges.

---
+++ src/bitBeat/QCC/JDBC.java
@@ -0,0 +1,8 @@
+package bitBeat.QCC;
+
+/**
+ * Created by sure GM on 2016/8/4 0:44.
+ */
+public class JDBC {
+
+}
--- src/bitBeat/QCC/POI.java
+++ src/bitBeat/QCC/POI.java
@@ -1,30 +1,69 @@
 package bitBeat.QCC;
 
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.*;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-
-import static bitBeat.YunYangHighSchool.FileSaver.wb;
 
 /**
  * Created by sure GM on 2016/8/3 23:43.
  */
 public class POI {
 
-	public void main (String[] args) throws Exception {
+	public static void main (String[] args) throws Exception {
 		File file = new File("D://yyzx.xlsx");
-		InputStream is = new FileInputStream(file);
-//		XSSFWorkbook wb = new XSSFWorkbook(is);
-		Workbook workbook = WorkbookFactory.create(is); //这种方式 Excel 2003/2007/2010 都是可以处理
+		InputStream is = new FileInputStream(file); // 获取输入流
+//		Workbook workbook = WorkbookFactory.create(is); // 这种方式 Excel 2003/2007/2010 都可以处理
+		XSSFWorkbook wb = new XSSFWorkbook(is); // 2007格式XSSF，2003格式HSSF
 
-		for (int i = 0; i < wb.getNumberOfSheets(); i++) {
-			if (wb.getSheetAt(i).is)
+		for (int numSheet = 0; numSheet < wb.getNumberOfSheets(); numSheet++) {
+			if (0 == wb.getSheetAt(numSheet).getPhysicalNumberOfRows()) {
+				continue;
+			} else {
+				XSSFSheet sheet = wb.getSheetAt(numSheet);
+
+
+
+				for (int numRow = 0; numRow < sheet.getLastRowNum(); numRow++) {
+					if (sheet.getRow(numRow).getPhysicalNumberOfCells() == 0) {
+						continue;
+					} else {
+						XSSFRow row = sheet.getRow(numRow);
+						for (int numCell = 0; numCell < row.getLastCellNum(); numCell++) {
+							XSSFCell cell = row.getCell(numCell);
+
+						}
+					}
+				}
+			}
 		}
 	}
 
+	/**
+	 * 删除标记有删除线的行
+	 * @param sheet
+	 */
+	public void deleteStrikeRows(XSSFSheet sheet) {
+		for (int numRow = 0; numRow < sheet.getLastRowNum(); numRow++) {
+			if (sheet.getRow(numRow).getPhysicalNumberOfCells() == 0) {
+				continue;
+			} else {
+				XSSFRow row = sheet.getRow(numRow);
+				for (int numCell = 0; numCell < row.getLastCellNum(); numCell++) {
+					XSSFCell cell = (XSSFCell) row.getCell(numCell);
+					XSSFCellStyle style = cell.getCellStyle();
+					XSSFFont font = style.getFont();
+					boolean isStrikeout = font.getStrikeout();
 
+					if (isStrikeout) {
+						sheet.removeRow(row);
+						sheet.shiftRows(numRow + 1, sheet.getLastRowNum(), -1);
+						numRow--;   // 避免跳行
+					}
+				}
+			}
+		}
+	}
 
 }
