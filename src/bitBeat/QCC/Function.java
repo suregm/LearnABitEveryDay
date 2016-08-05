26ef04fc5ab46440cdf6c686ab29dca5b675def3
suregm <sure0kuaile@qq.com>
2016-08-06 01:31:47 +0800

    QCC knowledges. customize Contains().

--- src/bitBeat/QCC/Function.java
+++ src/bitBeat/QCC/Function.java
@@ -1,5 +1,9 @@
 package bitBeat.QCC;
 
+
+import org.apache.poi.ss.usermodel.Cell;
+
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,9 +36,6 @@ public class Function {
 		}
 		return result;
 	}
-
-
-
 
 	/**
 	 * 自定义contains方法，以验证基线字符串涵盖多种可能的情况
@@ -81,5 +82,37 @@ public class Function {
 			}
 		}
 		return result;
+	}
+
+	/**
+	 * 得到单元格不同类型的值
+	 * @param cell
+	 * @return
+	 */
+	public static String getStringNumberCellValue(Cell cell) {
+		String value = null;
+		NumberFormat nf = NumberFormat.getInstance();
+		switch (cell.getCellType()) {
+			case Cell.CELL_TYPE_NUMERIC:
+				value = nf.format(cell.getNumericCellValue());
+				break;
+			case Cell.CELL_TYPE_STRING:
+				value = nf.format(cell.getStringCellValue());
+				break;
+			case Cell.CELL_TYPE_BOOLEAN:
+				value = nf.format(cell.getBooleanCellValue());
+				break;
+			case Cell.CELL_TYPE_FORMULA:
+				value = nf.format(cell.getCellFormula());
+				break;
+			case Cell.CELL_TYPE_BLANK:
+				value = "";
+				break;
+			default:
+				System.out.println(cell.getCellType());
+				break;
+		}
+
+		return value;
 	}
 }
