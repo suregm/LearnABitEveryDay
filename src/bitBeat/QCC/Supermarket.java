baba398fe8138760ff68b33fb7b38b6508b4fbdf
suregm <sure0kuaile@qq.com>
2016-08-08 00:06:30 +0800

    QCC knowledges. debug the contains_GoMX(), contains_Sure().

--- src/bitBeat/QCC/Supermarket.java
+++ src/bitBeat/QCC/Supermarket.java
@@ -11,12 +11,12 @@ public class Supermarket {
 //		this.supermarket = mSupermarket;
 
 
-//		// 高中时 十堰 2005 - 2008
-//		this.supermarket = "老虎沟寿康永乐1+1超市";
-//		// 大学时 武汉 2008 - 2012
-//		this.supermarket = "南湖学府平价，鉴湖教育超市，南湖沃尔玛";
-//		// 回家过年时
-//		this.supermarket = "房县寿康永乐生活广场";
+		// 高中时 十堰 2005 - 2008
+		this.supermarket = "老虎沟寿康永乐1+1超市";
+		// 大学时 武汉 2008 - 2012
+		this.supermarket = "南湖学府平价，鉴湖教育超市，南湖沃尔玛";
+		// 回家过年时
+		this.supermarket = "房县寿康永乐生活广场";
 		// 毕业后 深圳 2012 - 2015
 		this.supermarket = "学府路人人乐、岁宝百货、家乐福，大浪江南百货";
 		// 毕业后 咸宁 2015
---
+++ src/bitBeat/QCC/XML.java
@@ -0,0 +1,8 @@
+package bitBeat.QCC;
+
+/**
+ * Created by sure GM on 2016/8/8 0:00.
+ */
+public class XML {
+
+}
