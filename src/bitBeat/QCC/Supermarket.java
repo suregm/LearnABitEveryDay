ae2ed47e5fadaf9961212e5da881908c3e1dad44
suregm <sure0kuaile@qq.com>
2016-08-07 23:59:28 +0800

    QCC knowledges. debug the contains_GoMX(), contains_Sure().

--- src/bitBeat/QCC/Supermarket.java
+++ src/bitBeat/QCC/Supermarket.java
@@ -5,9 +5,9 @@ package bitBeat.QCC;
  */
 public class Supermarket {
 
-	public Supermarket supermarket;
+	public String supermarket;
 
-//	Supermarket(Supermarket mSupermarket) {
+	Supermarket(String mSupermarket) {
 //		this.supermarket = mSupermarket;
 
 
@@ -17,15 +17,15 @@ public class Supermarket {
 //		this.supermarket = "南湖学府平价，鉴湖教育超市，南湖沃尔玛";
 //		// 回家过年时
 //		this.supermarket = "房县寿康永乐生活广场";
-//		// 毕业后 深圳 2012 - 2015
-//		this.supermarket = "学府路人人乐、岁宝百货、家乐福，大浪江南百货";
-//		// 毕业后 咸宁 2015
-//		this.supermarket = "通城县世纪华联";
-//		// 毕业后 武汉 2015 - 2016
-//		this.supermarket = "珞狮路中百仓储";
-//		// 毕业后 武汉 2016 -
-//		this.supermarket = "杨家湾武商量贩";
+		// 毕业后 深圳 2012 - 2015
+		this.supermarket = "学府路人人乐、岁宝百货、家乐福，大浪江南百货";
+		// 毕业后 咸宁 2015
+		this.supermarket = "通城县世纪华联";
+		// 毕业后 武汉 2015 - 2016
+		this.supermarket = "珞狮路中百仓储";
+		// 毕业后 武汉 2016 -
+		this.supermarket = "杨家湾武商量贩";
 
-//	}
+	}
 
 }
