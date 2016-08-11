edbf8b773277cf0fdc78c1e890e4182181f3d213
suregm <sure0kuaile@qq.com>
2016-08-11 23:39:45 +0800

    QCC knowledges. POI.

--- src/bitBeat/QCC/POI.java
+++ src/bitBeat/QCC/POI.java
@@ -1,17 +1,19 @@
 package bitBeat.QCC;
 
-import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.*;
 
-import java.awt.*;
+import java.awt.Color;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by sure GM on 2016/8/3 23:43.
@@ -240,6 +242,126 @@ public class POI {
 		styleRegion.setAlignment(CellStyle.ALIGN_CENTER);
 		styleRegion.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
 		cellRegion.setCellStyle(styleRegion);
+	}
+
+	/**
+	 * create a library of cell styles
+	 */
+	private static Map<String, CellStyle> createStyles(Workbook wb){
+		Map<String, CellStyle> styles = new HashMap<String, CellStyle>();
+		DataFormat df = wb.createDataFormat();
+
+		CellStyle style;
+		Font headerFont = wb.createFont();
+		headerFont.setBoldweight(Font.BOLDWEIGHT_BOLD);
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_CENTER);
+		style.setFillForegroundColor(IndexedColors.LIGHT_CORNFLOWER_BLUE.getIndex());
+		style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+		style.setFont(headerFont);
+		styles.put("header", style);
+
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_CENTER);
+		style.setFillForegroundColor(IndexedColors.LIGHT_CORNFLOWER_BLUE.getIndex());
+		style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+		style.setFont(headerFont);
+		style.setDataFormat(df.getFormat("d-mmm"));
+		styles.put("header_date", style);
+
+		Font font1 = wb.createFont();
+		font1.setBoldweight(Font.BOLDWEIGHT_BOLD);
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_LEFT);
+		style.setFont(font1);
+		styles.put("cell_b", style);
+
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_CENTER);
+		style.setFont(font1);
+		styles.put("cell_b_centered", style);
+
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_RIGHT);
+		style.setFont(font1);
+		style.setDataFormat(df.getFormat("d-mmm"));
+		styles.put("cell_b_date", style);
+
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_RIGHT);
+		style.setFont(font1);
+		style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+		style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+		style.setDataFormat(df.getFormat("d-mmm"));
+		styles.put("cell_g", style);
+
+		Font font2 = wb.createFont();
+		font2.setColor(IndexedColors.BLUE.getIndex());
+		font2.setBoldweight(Font.BOLDWEIGHT_BOLD);
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_LEFT);
+		style.setFont(font2);
+		styles.put("cell_bb", style);
+
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_RIGHT);
+		style.setFont(font1);
+		style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+		style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+		style.setDataFormat(df.getFormat("d-mmm"));
+		styles.put("cell_bg", style);
+
+		Font font3 = wb.createFont();
+		font3.setFontHeightInPoints((short)14);
+		font3.setColor(IndexedColors.DARK_BLUE.getIndex());
+		font3.setBoldweight(Font.BOLDWEIGHT_BOLD);
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_LEFT);
+		style.setFont(font3);
+		style.setWrapText(true);
+		styles.put("cell_h", style);
+
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_LEFT);
+		style.setWrapText(true);
+		styles.put("cell_normal", style);
+
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_CENTER);
+		style.setWrapText(true);
+		styles.put("cell_normal_centered", style);
+
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_RIGHT);
+		style.setWrapText(true);
+		style.setDataFormat(df.getFormat("d-mmm"));
+		styles.put("cell_normal_date", style);
+
+		style = createBorderedStyle(wb);
+		style.setAlignment(CellStyle.ALIGN_LEFT);
+		style.setIndention((short)1);
+		style.setWrapText(true);
+		styles.put("cell_indented", style);
+
+		style = createBorderedStyle(wb);
+		style.setFillForegroundColor(IndexedColors.BLUE.getIndex());
+		style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+		styles.put("cell_blue", style);
+
+		return styles;
+	}
+
+	private static CellStyle createBorderedStyle(Workbook wb){
+		CellStyle style = wb.createCellStyle();
+		style.setBorderRight(CellStyle.BORDER_THIN);
+		style.setRightBorderColor(IndexedColors.BLACK.getIndex());
+		style.setBorderBottom(CellStyle.BORDER_THIN);
+		style.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+		style.setBorderLeft(CellStyle.BORDER_THIN);
+		style.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+		style.setBorderTop(CellStyle.BORDER_THIN);
+		style.setTopBorderColor(IndexedColors.BLACK.getIndex());
+		return style;
 	}
 
 }
