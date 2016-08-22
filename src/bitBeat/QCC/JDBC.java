da375da0fcdc38308cf74b7e5390b2e43d6f4074
suregm <sure0kuaile@qq.com>
2016-08-23 07:25:56 +0800

    QCC knowledges. JDBC.

--- src/bitBeat/QCC/JDBC.java
+++ src/bitBeat/QCC/JDBC.java
@@ -1,10 +1,11 @@
 package bitBeat.QCC;
 
 /**
+ * 感谢2015.11工作上接到的任务：开发一个工具，以处理数据库里获取的数据与本地Excel表格的数据并进行对比。
+ * 遂由此接触了JDBC，POI，XML解析，并且实践了swing界面。
  * Created by sure GM on 2016/8/4 0:44.
  */
 public class JDBC {
 
-	// 感谢2015.11工作上接到的任务：开发一个工具，以处理数据库里获取的数据与本地Excel表格的数据并进行对比。
-	// 遂由此接触了JDBC，POI，XML解析，并且实践了swing界面。
+
 }
