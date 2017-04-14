d4c727db32d91cf8b888930db600dd468893ca74
suregm <sure0kuaile@qq.com>
2017-04-14 23:47:57 +0800

    learn batch 002_batchRename.cmd

--- src/batch/001_batchCreateFile.bat
+++ src/batch/001_batchCreateFile.bat
@@ -1,14 +1,18 @@
 @echo off
 echo create files by BATCH.
+
 echo.
 echo.
 echo being creating file_0.txt ~ file_99.txt totally 100 files...
-echo.
-echo.
+
 for /l %%n in (0, 1, 99) do (
     echo file %%n >>file_%%n.txt
 )
+
+echo.
+echo.
 echo created complete.
+
 echo.
 echo.
 pause
--- src/batch/002_batchRename.cmd
+++ src/batch/002_batchRename.cmd
@@ -3,6 +3,7 @@ echo start to rename files's name...
 
 ::变量赋值非数值时等号两边不能含有空格
 set extension=.rar
+::参数/a表示数值类型变量
 set /a sum = 0
 
 ::文件名不能包含空格，可以是002_batchRenameCopy.cmd或者002_batchRename-Copy.cmd，否则报语法错误
