92036316ad5f382592fd7e7ee1597042aae82b4e
suregm <sure0kuaile@qq.com>
2017-06-07 23:37:40 +0800

    learn batch. delNullFolder.bat

--- src/batch/006_delNullFolder.cmd
+++ src/batch/006_delNullFolder.cmd
@@ -6,8 +6,13 @@ echo.
 
 echo deleting the empty folder...
 echo ----------------------------
+:: "cd."命令不显示任何内容，因此将该命令的结果利用“>”重定向操作符输出到文件，便创建一个空文件
 cd. >listnull.txt
+:: “dir /ad /b /s”表示显示当前目录及所有子目录中的文件夹，显示结果中每个文件夹信息占一行
 for /f "delims=" %%i in ('dir /ad /b /s') do (
+    :: “||”连接两个命令时，仅当符号前面的命令执行失败后，才继续执行符号后的命令
+    :: “dir /b "%%i"” 显示“%%i”目录中的内容，“/b”参数确保“%%i”为空目录时不显示任何内容
+    :: “|” 为管道符
     dir /b "%%i"|findstr .>nul || echo %%i >>listnull.txt
 )
 set /a sum=0
