ab5ab6ca09cf81cd27316aa9a7b5469d80d15bad
suregm <sure0kuaile@qq.com>
2017-07-16 18:47:28 +0800

    learn batch. replaceText.bat

--- src/batch/011_replaceText.cmd
+++ src/batch/011_replaceText.cmd
@@ -4,14 +4,19 @@ echo %0
 echo.
 echo.
 
-setlocal enabledelayexpansion
+:: 启用批处理变量延时扩充功能，当启用时，可以使用“!”批处理执行时扩充变量
+setlocal enabledelayedexpansion
+:: cd. 命令创建文件
 cd. >profit_new.doc
 echo being replacing...
 echo.
 
 for /f %%a in (profit.doc) do (
     set str=%%a
-    set str=!str:OTHER=OTHER!
+    :: 两个“!”之间的str使用了变量延时，先计算!!，将str变量中的“其它”替换为“其他”，然后将替换后的“str”赋值给原先的“str”变量
+    :: 若在延时符号“!”之间未使用“setlocal enabledelayedexpansion”命令开启延时变量支持功能，
+    :: 则延时符号“!”将不起任何作用，批处理只将其作为普通的字符处理。
+    set str=!str:其它=其他!
     echo !str! >>profit_new.doc
 )
 
