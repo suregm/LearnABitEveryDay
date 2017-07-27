6618cab8a021472d3b7dba03473b1d5020e0253f
suregm <sure0kuaile@qq.com>
2017-07-27 23:59:13 +0800

    learn batch. 015_restoreIE.cmd

--- src/batch/Desktop_and_IE/015_restoreIE.cmd
+++ src/batch/Desktop_and_IE/015_restoreIE.cmd
@@ -8,14 +8,16 @@ echo Windows Registry Editor Version 5.00 >temp.reg
 
 echo 正在恢复IE默认主页...
 echo [HKEY_USER\.DEFAULT\Software\Policies\Microsoft\Internet Explorer\Control Panel] >>temp.reg
+:: “homepage”值设置为“0”表示允许用户修改默认主页
 echo "homepage"="0" >>temp.reg
 echo [HKEY_CURRENT_USER\Softwar\Microsoft\Internet Explorer\Main] >>temp.reg
 echo "Start Page"="about:blank" >>temp.reg
-echo 正在恢复IE标题栏
+echo 正在恢复IE标题栏...
 echo "Window Title"="Microsoft Internet Explorer" >>temp.reg
-echo 正在清除登录时弹出的广告对话框...
 echo [HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Winlogon] >>temp.reg
+echo 正在清除登录时弹出的广告对话框...
 echo "LegalNotice-Caption"=- >>temp.reg
+echo 正在删除添加到IE右键菜单中的广告
 echo "LegalNoticeText"=- >>temp.reg
 echo 成功恢复IE默认设置！
 echo.
