88f0b309347b8102e8d879590b744c149d7de6c2
suregm <sure0kuaile@qq.com>
2017-07-29 19:07:11 +0800

    learn batch. 001_autoLogon.cmd

---
+++ src/batch/Windows_Performance/001_autoLogon.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+echo %0
+echo.
+echo.
+
+echo Windows Registry Editor Version 5.00 >temp.reg
+
+echo 正在设置自动登录当前系统的账户...
+echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon] >>temp.reg
+echo "Autoadminlogon"="1" >>temp.reg
+echo "Defaultusername"="NormalAccount" >>temp.reg
+echo "Defaultpassword"="NoPrivilege" >>temp.reg
+echo.
+echo 成功将“NormalAccount”账户设置为当前自动登录的帐户！
+regedit /s temp.reg
+del /q /f temp.reg >nul
