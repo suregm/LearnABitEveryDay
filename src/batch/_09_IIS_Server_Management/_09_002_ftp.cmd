919acedd3ca7c6ac185fd10195e692f5d1320268
suregm <sure0kuaile@qq.com>
2017-08-07 23:30:52 +0800

    learn batch. _09_005_web.cmd

--- src/batch/_09_IIS_Server_Management/_09_002_ftp.cmd
+++ src/batch/_09_IIS_Server_Management/_09_002_ftp.cmd
@@ -8,7 +8,9 @@ echo ---------请选择要执行的操作---------
 echo ----1.输入数字1并回车，创建普通的FTP站点
 echo ----2.输入数字2并回车，创建具有指定IP地址和端口的FTP站点
 echo ----3.输入数字3并回车，删除指定的FTP站点
-echo ----4.输入其他字符，直接退出批处理
+echo ----4.输入数字4并回车，为FTP站点创建虚拟目录
+echo ----5.输入数字5并回车，删除FTP站点中的虚拟目录
+echo ----6.输入其他字符，直接退出批处理
 echo.
 echo.
 
@@ -21,9 +23,9 @@ if "%num%"=="1" (
 
     :: iisftp /create
     :: iisftp /query
-    :: iisftp /start
-    :: iisftp /pause
-    :: iisftp /stop
+    :: iisftp /start %siteName%
+    :: iisftp /pause %siteName%
+    :: iisftp /stop %siteName%
 
     iisftp /create %sourcePath% %siteName%
     goto exit
@@ -47,6 +49,28 @@ if "%num%"=="3" (
     iisftp /delete %removedSite%
     goto exit
 )
+if "%num%"=="4" (
+    cls
+    iisftp /query
+    set /p siteName=请输入要创建虚拟目录的FTP站点名称：
+    set /p sourceVD=请输入虚拟目录的资源路径：
+    set /p vdName=请输入虚拟目录的名称：
+    echo.
+    :: 创建虚拟目录
+    iisftpdr /create %siteName% %vdName% %sourceVD%
+    goto exit
+)
+if "%num%"=="2" (
+    cls
+    iisftp /query
+    set /p siteName=请输入要删除虚拟目录的FTP站点：
+    echo.
+    iisftpdr /query %siteName%
+    set /p vdName=请输入要删除的虚拟目录：
+    echo.
+    iisftpdr /delete %siteName%\%vdName%
+    goto exit
+)
 echo.
 echo 你输入了%num%字符，批处理自动退出...
 
@@ -57,3 +81,5 @@ set siteName=
 set ip=
 set port=
 set removedSite=
+set sourceVD=
+set vdName=
---
+++ src/batch/_09_IIS_Server_Management/_09_005_web.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+echo %0
+echo.
+echo.
+
+:: 通过创建web站点，用户可以发布企业或个人的主页、Web应用程序等。
+
