934068853f739e5e60131b27b6c9c7cfcfe52380
suregm <sure0kuaile@qq.com>
2017-11-21 23:56:47 +0800

    learning. Core Java $001.

---
+++ src/learn/CoreJava/$001_Java.java
@@ -0,0 +1,15 @@
+package learn.CoreJava;
+
+public class $001_Java {
+
+    // Simple
+    // Objected-Oriented
+    // Distributed
+    // Robust
+    // Secure
+    // Architecture-Neutral
+    // Portable
+    // Interpreted
+
+
+}
