f7ec8d018dd5fd53cc51da2100010d231397f112
suregm <sure0kuaile@qq.com>
2019-03-01 23:42:51 +0800

    JCheckBox JScrollPane test.

--- src/learn/TestGUI/TestEvent.java
+++ src/learn/TestGUI/TestEvent.java
@@ -14,6 +14,9 @@ public class TestEvent extends JFrame implements ActionListener {
     ArrayList<JCheckBox> cbs = new ArrayList<>();
     JCheckBox cbAll;
     JCheckBox cb;
+    JButton btn;
+    JScrollPane scrollPane;
+    JPanel panel;
 
     public TestEvent() {
         super("TestJScrollPane");
@@ -21,19 +24,47 @@ public class TestEvent extends JFrame implements ActionListener {
         this.setBounds(0, 0, 400, 400);
 
 
-        JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setBounds(10, 10, 300, 300);
+//        scrollPane = new JScrollPane();
+//        scrollPane.setBounds(10, 10, 300, 300);
 
 
-        JPanel panel = new JPanel();
+
+
+
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        btn = new JButton("click");
+        btn.setBounds(20, 330, 300, 20);
+        btn.addActionListener(this);
+        this.add(btn);
+
+
+
+    }
+
+    private void addCheckBox() {
+        cbs.clear();
+
+        if(panel != null) {
+            this.remove(panel);
+        }
+        if(scrollPane != null) {
+            this.remove(scrollPane);
+        }
+
+        panel = new JPanel();
 //        panel.setPreferredSize(new Dimension(200,100));//主要是这句代码，设置panel的首选大小，同时保证宽高大于JScrollPane的宽高，这样下面的JScrollPane才会出现滚动条
         panel.setLayout(new GridLayout(18,1, 5, 5));
+
+
 
         cbAll = new JCheckBox("All");
         cbAll.setSelected(true);
         cbAll.setVisible(true);
         panel.add(cbAll);
         cbs.add(cbAll);
+        cbAll.addActionListener(this);
 
         for(int i = 0; i < 18; i++) {
             cb = new JCheckBox("dddd" + i);
@@ -44,13 +75,12 @@ public class TestEvent extends JFrame implements ActionListener {
             cb.addActionListener(this);
         }
 
+        this.add(panel);
+
         scrollPane = new JScrollPane(panel);
         scrollPane.setBounds(10, 10, 300, 300);
         this.getContentPane().add(scrollPane);
-        this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        cbAll.addActionListener(this);
+        scrollPane.setVisible(true);
     }
 
     public static void main(String[] args) {
@@ -59,7 +89,13 @@ public class TestEvent extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == cbAll) {
+
+        if(e.getSource() == btn) {
+            addCheckBox();
+            System.out.println(cbs.size());
+        }
+
+        else if(e.getSource() == cbAll) {
             System.out.println(cbs.size());
 
             for(JCheckBox cb : cbs) {
@@ -70,7 +106,7 @@ public class TestEvent extends JFrame implements ActionListener {
                 }
             }
 
-        } else if(!((JCheckBox)e.getSource()).getText().equals("All")) {
+        } else if(e.getSource().getClass().toString().contains("javax.swing.JCheckBox")) {
             boolean allSelect = true;
 
             for(JCheckBox cb : cbs) {
